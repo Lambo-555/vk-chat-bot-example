1d618f7b1ebc57dac713200dc45c3c488a3b27e5
lambo-555 <lambo-555@yandex.ru>
2019-03-28 10:59:48 +0300

    console add

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ console.log('SERVER STAS IS RUNNIG 1');
 var express = require('express');
 var vk = require('vk-chat-bot');
 ////////////////////////////////////////////////
-const MongoClient = require(‘mongodb’).MongoClient;
-const vice_password = "andramedan1"
+const MongoClient = require('mongodb').MongoClient;
+const vice_password = "andramedan1";
 const uri = "mongodb+srv://Stan:"+vice_password+"@cluster0-tqhov.mongodb.net/test?retryWrites=true";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 client.connect(err => {
@@ -13,11 +13,10 @@ client.connect(err => {
   client.close();
 });
 
-/////////////////////
-var app = express();
+/////////////////////// xxxxx
+var app = express();/// x(1)x
+/////////////////////// xxxxx
 
-
-////////////////////////////////////////////////
 const params = {
     vkToken: '62cf2012c62371026d7a3277fc94d608fbfa754da100899e1e55d6bac8127d6217afd27033172d0d80eef',
     confirmationToken: 'b9b7a889',
